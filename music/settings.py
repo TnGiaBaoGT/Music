cf0593c9ff0ee9c2da1351c914d3f0c344cac50a
TnGiaBaoGT <115094484+TnGiaBaoGT@users.noreply.github.com>
2024-05-22 01:07:53 +0700

    Update settings.py

--- music/settings.py
+++ music/settings.py
@@ -131,7 +131,8 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
