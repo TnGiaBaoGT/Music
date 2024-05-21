cab32349aa5dbcd70a1f571ad91d3b9d1d6ec64e
TnGiaBaoGT <115094484+TnGiaBaoGT@users.noreply.github.com>
2024-05-22 00:48:13 +0700

    Update settings.py

--- music/settings.py
+++ music/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 # STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
