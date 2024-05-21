fb2f3c11ba16627e5844279f1bc6a889fe15118d
TnGiaBaoGT <115094484+TnGiaBaoGT@users.noreply.github.com>
2024-05-22 02:08:33 +0700

    Update settings.py

--- music/settings.py
+++ music/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # STATIC_URL = 'static/'
