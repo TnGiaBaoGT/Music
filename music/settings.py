4db68889a957b1f38c96a944a7c75d340b37201f
TnGiaBaoGT <115094484+TnGiaBaoGT@users.noreply.github.com>
2024-05-22 11:01:29 +0700

    Update settings.py

--- music/settings.py
+++ music/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-
+from django.core.files.storage import FileSystemStorage
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,5 +154,8 @@ CORS_ALLOW_ALL_HEADERS=True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Media configurations
+
+MEDIA_ROOT = '/tmp/media'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_FILE_STORAGE = 'myapp.custom_storages.TemporaryFileSystemStorage'
