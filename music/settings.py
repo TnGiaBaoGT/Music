e8265b262fc4ffad845a24ba13efdc497d5cc008
TnGiaBaoGT <115094484+TnGiaBaoGT@users.noreply.github.com>
2024-05-22 11:04:31 +0700

    Update settings.py

--- music/settings.py
+++ music/settings.py
@@ -158,4 +158,5 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_ROOT = '/tmp/media'
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-DEFAULT_FILE_STORAGE = 'myapp.custom_storages.TemporaryFileSystemStorage'
+os.makedirs(MEDIA_ROOT, exist_ok=True)
+DEFAULT_FILE_STORAGE = 'MusicApp.custom_storages.TemporaryFileSystemStorage'
