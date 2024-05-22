2301db214d3c07c981122fa28640f877b434025c
TnGiaBaoGT <115094484+TnGiaBaoGT@users.noreply.github.com>
2024-05-22 11:07:49 +0700

    Update apps.py

--- MusicApp/apps.py
+++ MusicApp/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
-
+import os
 
 class MusicappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'MusicApp'
+def ready(self):
+        # Ensure the /tmp/media directory and subdirectories exist
+        os.makedirs('/tmp/media/music_files', exist_ok=True)
+        os.makedirs('/tmp/media/music_images', exist_ok=True)
+ 
