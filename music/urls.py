7223b02d5a2204d5fb1aff9399bc69aa945e476a
TnGiaBaoGT <115094484+TnGiaBaoGT@users.noreply.github.com>
2024-06-03 11:56:54 +0700

    Update urls.py

--- music/urls.py
+++ music/urls.py
@@ -45,6 +45,9 @@ urlpatterns = [
     
     path('albums/', views.albumApi),
     path('albums/<int:id_album>', views.albumApi),
+
+    path('purchases/', views.purchaseApi),
+    path('purchase/<int:id_purchase>', views.purchaseApi),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
