6dc0321129746fd6833e8f4cae2fc30c2fa3744f
TnGiaBaoGT <115094484+TnGiaBaoGT@users.noreply.github.com>
2024-06-29 23:16:39 +0700

    Update urls.py

--- music/urls.py
+++ music/urls.py
@@ -84,6 +84,9 @@ urlpatterns = [
     path('composer_earnings/', views.composer_earnings_api),
     path('composer_earnings/<int:id_composer_earnings>/', views.composer_earnings_api),
     path('composer_earnings/composer/<int:id_composer>/', views.composer_earnings_api),
+
+    path('composer-earnings-detail/', views.composer_earnings_detail),
+    path('composer-earnings-detail/<int:composer_earnings_id>/', views.composer_earnings_detail_api),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
