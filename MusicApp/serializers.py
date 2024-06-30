1ae31988212af50d869c762b476a3c3e0c79af4f
TnGiaBaoGT <115094484+TnGiaBaoGT@users.noreply.github.com>
2024-06-30 16:24:06 +0700

    Update serializers.py

--- MusicApp/serializers.py
+++ MusicApp/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from MusicApp.models import Music, User, Singer, Vote, Transaction, Album, Purchase, Like, MusicBundle, BundlePurchase,MusicCart,MusicPurchased,MusicPurchasedItem,ComposerEarnings,ComposerEarningsDetail
+from MusicApp.models import Music, User, Singer, Vote, Transaction, Album, Purchase, Like, MusicBundle, BundlePurchase,MusicCart,MusicPurchased,MusicPurchasedItem,ComposerEarnings,ComposerEarningsDetail,Ads
 from datetime import timedelta
 from django.utils import timezone
 
@@ -115,4 +115,9 @@ class ComposerEarningsDetailSerializer(serializers.ModelSerializer):
     composer_earnings_detail = ComposerEarningsSerializer(source="composer_earnings",read_only=True)
     class Meta:
         model = ComposerEarningsDetail
+        fields = '__all__'
+
+class AdsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ads
         fields = '__all__'
