8fcb0fcaca877b7e4c2055d1060558f6f7c9216e
TnGiaBaoGT <115094484+TnGiaBaoGT@users.noreply.github.com>
2024-07-22 20:03:28 +0700

    Update urls.py

--- music/urls.py
+++ music/urls.py
@@ -46,6 +46,7 @@ urlpatterns = [
     path('albums/', views.albumApi),
     path('albums/<int:id_album>', views.albumApi),
     path('albums/user/<int:id_user>', views.albumApi),
+    path('remove_music_from_album/', views.remove_music_from_album),
 
     path('purchases/', views.purchaseApi),
     path('purchases/<int:id_purchase>', views.purchaseApi),
