c00723a7db90b6e0e52ac8aa5eabc64aac921c78
TnGiaBaoGT <115094484+TnGiaBaoGT@users.noreply.github.com>
2024-08-01 14:18:01 +0700

    Update models.py

--- MusicApp/models.py
+++ MusicApp/models.py
@@ -270,7 +270,7 @@ class ComposerEarningsDetail(models.Model):
     view_count = models.IntegerField(default=0)
     total_earnings = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     bank_account = models.OneToOneField(BankAccount, on_delete=models.CASCADE, null=True, blank=True)
-    withdrawal_date = models.DateTimeField(null=True, blank=True,auto_now_add=True) 
+    withdrawal_date = models.DateTimeField(null=True, blank=True) 
     status_state = models.BooleanField(default=False)
 
     def calculate_total_earnings(self):
